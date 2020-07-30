e611278befe343570ba18a27a67c5fd937f6fcc9
Bianca Reusch <reusch.bianca@gmail.com>
2020-07-30 15:28:18 -0500

    exercise 2 finished

---
+++ js/break_and_continue.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+//Exercise 2
+while(true){
+    var userInput = prompt("Type in a Number between 1 and 50!");
+    if(userInput >= 1 && userInput <= 50) {
+        alert("Thank you! Your number is " + userInput);
+        break;
+    }else {
+        alert("Not a valid entry. Try again.");
+        }
+}
+
+//Exercise 3
+
