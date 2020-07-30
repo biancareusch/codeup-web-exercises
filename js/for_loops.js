050699b17b71f179f93dc90ef8ac723297cfb384
Bianca Reusch <reusch.bianca@gmail.com>
2020-07-30 14:12:18 -0500

    exercise 5 finished

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,8 +1,6 @@
 "use strict";
-//random Num between 50 and 200
 
-
-
+//exercise 2
 function showMultiplicationTable(input){
 for(var x = 1; x <= 10; x++){
     console.log(x + " * " + input + " = " + (x * input));
@@ -10,7 +8,7 @@ for(var x = 1; x <= 10; x++){
 }}
 showMultiplicationTable(7);
 
-
+//exercise 3
 for(var x = 1; x <=10; x++){
     var randomNum = Math.floor(Math.random() * 150) + 50;
     if(randomNum % 2 === 0){
@@ -20,10 +18,19 @@ for(var x = 1; x <=10; x++){
     }
 }
 
-var oneTwo = 1;
+//exercise 4
+// var oneTwo = 1;
+//
+// for(x = 1; x < 10; x++){
+//     for(oneTwo = 1; oneTwo < 10; oneTwo++){
+//         console.log(x);
+//     }
+// }
 
-for(x = 1; x < 10; x++){
-    for(oneTwo = 1; oneTwo < 10; oneTwo++){
-        console.log(x);
-    }
+//exercise 5
+for(var x = 100; x >= 5 ; x = x - 5){
+    console.log(x);
 }
+
+
+
