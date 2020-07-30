d15e55ad3a50d4335e52beda4e00e47af0917ba6
Bianca Reusch <reusch.bianca@gmail.com>
2020-07-30 16:54:00 -0500

    still working on exercise 3

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -2,15 +2,39 @@
 
 
 //Exercise 2
-while(true){
-    var userInput = prompt("Type in a Number between 1 and 50!");
-    if(userInput >= 1 && userInput <= 50) {
-        alert("Thank you! Your number is " + userInput);
-        break;
-    }else {
-        alert("Not a valid entry. Try again.");
+function numTo50() {
+    while (true) {
+        var userInput = prompt("Type in a Number between 1 and 50!");
+        if (userInput >= 1 && userInput <= 50) {
+            alert("Thank you! Your number is " + userInput);
+            break;
+        } else {
+            alert("Not a valid entry. Try again.");
         }
+    }
 }
+numTo50();
+
 
 //Exercise 3
+function odd50() {
+    while (true) {
+        var userInput = prompt("Type in an odd Number between 1 and 50!");
+        if (userInput >= 1 && userInput <= 50 && userInput % 2 !== 0) {
+            alert("Thank you! Your number is " + userInput);
+            console.log("Number to skip is: " + userInput);
+            return userInput;
+            break;
+        } else {
+            alert("Not a valid entry. Try again.");
+        }
+    }
+}
 
+odd50();
+ for( var i = 1; i < 50; i = i + 2){
+        console.log("Here's an odd number: "+ i);
+    // }else {
+    //    console.log("Yikes Skipping number: " + odd50());
+    // }
+ }
