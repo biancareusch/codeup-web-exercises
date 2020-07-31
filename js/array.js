fff21e2f5dd22090bc2b9539d524d04777a40b68
Bianca Reusch <reusch.bianca@gmail.com>
2020-07-31 12:21:35 -0500

    exploring arrays

---
+++ js/array.js
@@ -0,0 +1,30 @@
+"use strict";
+//practice
+// var arr = [1,2,3,4,5];
+//
+// var arr2 = [true, false];
+//
+// var newArr = arr + arr2;
+//
+// console.log(newArr);
+// console.log(newArr[5]);
+// console.log(typeof(newArr)); //newArr is a string now
+//
+// var newArr2 = arr.concat(arr2);
+// console.log(newArr2);
+// console.log(typeof(newArr2)); //newArr2 is an object
+//
+// var arr3 = [arr + arr2];
+// console.log(arr3);
+// console.log(typeof(arr3));
+//
+//
+// var vacay = ["Sydney", "Virginia Beach", "London", "Las Vegas", "Perth", "Toronto"];
+//
+// console.log(vacay[vacay.length-1]);
+// // ^ same as:
+// console.log(vacay.pop());
+//
+// //add to the end of array:
+// console.log(vacay.push("Punta Cana"));
+// console.log((vacay));
