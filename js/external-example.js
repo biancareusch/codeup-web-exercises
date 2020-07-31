6fdb7ee19208d45b6bc7dc3b60b329921eb02d64
Bianca Reusch <reusch.bianca@gmail.com>
2020-07-31 16:45:24 -0500

    javascript for first exercises

---
+++ js/external-example.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log("external works!");
+//alert("External Works!")
+
+//var response = confirm("Is it raining?")
+//console.log("response",response);
+
+var name = prompt("What's your name?");
+console.log(typeof(name));
+console.log("name", name);
