a348d00d8e5c9f9182339374b32c6a9ce1665f7c
Bianca Reusch <reusch.bianca@gmail.com>
2020-09-17 16:57:30 -0500

    got the cards back

--- js/weather_api.js
+++ js/weather_api.js
@@ -1,4 +1,4 @@
-(function () {
+$(document).ready(function () {
     "use strict";
     mapboxgl.accessToken = mapboxToken;
 
@@ -10,62 +10,62 @@
         zoom: 11, // starting zoom
     });
 
-    // GET WEATHER API ( coordinates)
-    function weatherCoordinates(lng, lat) {
-        $.get("http://api.openweathermap.org/data/2.5/forecast", {
+    //MARKER
+
+    map.on('click', function (e) {
+        // getting latitude and longitude for weather app
+        let clickCoordinates = e.lngLat;
+        let marker = new mapboxgl.Marker()
+            .setLngLat(clickCoordinates)
+            .addTo(map);
+    })
+
+
+    // GET WEATHER API
+    const openWeatherURL = "http://api.openweathermap.org/data/2.5/forecast";
+
+        $.get(openWeatherURL, {
             APPID: openWeatherKey,
             // lon: lng,
-            lon:-64.969414,
+            q: "Saint Thomas",
+            // lon: -64.969414,
             // lat: lat,
-            lat:18.327129,
+            // lat: 18.327129,
             units: "imperial"
         }).done(function (data) {
-                //DRAW HEADER
-                let headerHTML = '<h1>' + data.city.name + '</h1>';
-                $('.location').append(headerHTML);
+            //DRAW HEADER
+            console.log('done');
+            let headerHTML = '<h1>' + data.city.name + '</h1>';
+            $('.location').append(headerHTML);
 
-                //DRAW WEATHER CARDS
-                for (let i = 0; i <= data.list.length - 1; i = i + 8) {
-                    let date = data.list[i].dt_txt.substring(0, 10);
-                    let iconURL = "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png";
-                    console.log(data);
-                    let finalHTML =
-                        '<div class="cards">' +
-                        '<div class="card text-center">' +
-                        '<div class="card-header">' + date + '</div>' +
-                        '<div class="card-body">' +
-                        '<ul class="list-group list-group-flush">' +
-                        '<li class="list-group-item">' + '<p class="font-weight-bold">' + data.list[i].main.temp + ' °F' + '</p>' + '<div><img id="img" class="img" alt=""></div>' + '</li>' +
-                        '<li class="list-group-item">' + 'Description: ' + '<p class="font-weight-bold">' + data.list[i].weather[0].description + '</p>' + '</li> ' +
-                        '<li class="list-group-item">' + 'Humidity: ' + '<p class="font-weight-bold">' + data.list[i].main.humidity + '</p>' + '</li>' +
-                        '<li class="list-group-item">' + 'Wind: ' + '<p class="font-weight-bold">' + +data.list[i].wind.speed + '</p>' + '</li>' +
-                        '<li class="list-group-item">' + 'Pressure: ' + '<p class="font-weight-bold">' + data.list[i].main.pressure + '</p>' + '</li>' +
-                        '</ul>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>';
+            //DRAW WEATHER CARDS
+            for (let i = 0; i <= data.list.length - 1; i = i + 8) {
+                let date = data.list[i].dt_txt.substring(0, 10);
+                let iconURL = "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png";
+                console.log(data);
+                let finalHTML =
+                    '<div class="cards">' +
+                    '<div class="card text-center">' +
+                    '<div class="card-header">' + date + '</div>' +
+                    '<div class="card-body">' +
+                    '<ul class="list-group list-group-flush">' +
+                    '<li class="list-group-item">' + '<p class="font-weight-bold">' + data.list[i].main.temp + ' °F' + '</p>' + '<div><img id="img" class="img" alt=""></div>' + '</li>' +
+                    '<li class="list-group-item">' + 'Description: ' + '<p class="font-weight-bold">' + data.list[i].weather[0].description + '</p>' + '</li> ' +
+                    '<li class="list-group-item">' + 'Humidity: ' + '<p class="font-weight-bold">' + data.list[i].main.humidity + '</p>' + '</li>' +
+                    '<li class="list-group-item">' + 'Wind: ' + '<p class="font-weight-bold">' + +data.list[i].wind.speed + '</p>' + '</li>' +
+                    '<li class="list-group-item">' + 'Pressure: ' + '<p class="font-weight-bold">' + data.list[i].main.pressure + '</p>' + '</li>' +
+                    '</ul>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>'
 
-                    $('#weather').append(finalHTML);
-                    $('.img').attr('src', iconURL);
-                }
-
-                let markerCoordinates =
-                    map.on('click', function (e) {
-                        // getting latitude and longitude for weather app
-                        var coordsObj = e.lngLat;
-                        // console.log(coordsObj);
-                        // console.log('data.city' + data.city.coord.lat);
-                        console.log('markercoord ' + coordsObj.lng);
-                        var marker = new mapboxgl.Marker()
-                            .setLngLat(coordsObj)
-                            .addTo(map);
-                        // data.city.coord.lat = coordsObj.lat;
-                        // data.city.coord.lon = coordsObj.lng;
-
-                    })
+                $('#weather').append(finalHTML);
+                $('.img').attr('src', iconURL);
             }
-        )
-    }
+
+
+        })
+
 
 
     //TODO fix icons, make all cards the same size, update weather to marker coordinates, make search bar functional
