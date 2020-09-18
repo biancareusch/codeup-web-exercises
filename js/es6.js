8f189872246a3961f6355b40afea64186880f50c
Bianca Reusch <reusch.bianca@gmail.com>
2020-09-18 12:31:17 -0500

    added filter/ map exercise functions

--- js/es6.js
+++ js/es6.js
@@ -63,7 +63,7 @@ users.forEach((user) =>{
 
 //  replace `var` with `let` in the following declaration
 let developers = [];
-users.forEach(function({name, email, languages}) {
+users.forEach((user) => {
   // rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
   //       parameter definition
