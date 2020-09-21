4b3dfcda320e3dc289cab3da7b41de78eccd3b2f
Bianca Reusch <reusch.bianca@gmail.com>
2020-09-21 16:48:00 -0500

    finished

--- js/promises.js
+++ js/promises.js
@@ -1,24 +1,32 @@
 "use strict";
 
 
-fetch('https://api.github.com/repos/biancareusch/codeup-web-exercises', {headers: {'Authorization': gitkey}})
-    .then(response => {
-        response.json()
-            .then(data => {
-                    console.log('Last commit : ' + data.created_at)
-                }
-                // author: json.commit.author.login,
-                // date: json.commit.commit.author.date,
-            );
-    })
-    .catch(error => console.log(error));
+// fetch('https://api.github.com/repos/biancareusch/codeup-web-exercises', {headers: {'Authorization': gitkey}})
+//     .then(response => {
+//         response.json()
+//             .then(data => {
+//                     console.log('Last commit : ' + data.created_at)
+//                 }
+//                 // author: json.commit.author.login,
+//                 // date: json.commit.commit.author.date,
+//             );
+//     })
+//     .catch(error => console.log(error));
 
 
 const findUser = (name) => {
-    let userURL = `https://api.github.com/users/${name}/repos`
+    let userURL = `https://api.github.com/users/${name}/events`
     fetch(userURL, {headers: {'Authorization': gitkey}})
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            let commit = [];
+            for (let event of data){
+                if (event.type === "PushEvent"){
+                    commit.push(event.created_at);
+                }
+                console.log(commit);
+            }
+        })
         .catch(error => console.log(error))
 }
 
